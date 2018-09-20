45603df1f8590f72fae62b277ef60fb70ac12f82
Nikolay Rumenov <nikolayrumenovsf@gmail.com>
2018-09-20 17:59:20 +0300

    Refactored markup and styles

--- src/containers/nav/nav.js
+++ src/containers/nav/nav.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import config from '../../config.js';
+import './nav.css';
+
 import {
     Navbar,
     NavbarBrand,
@@ -24,13 +26,6 @@ class Nav extends React.Component {
         this.onClick = this.onClick.bind(this)
     }
 
-    onClick() {
-        console.log(arguments);
-        this.setState({
-            collapse: !this.state.collapse,
-        });
-    }
-
     render() {
         return (
             <Router>
@@ -39,7 +34,7 @@ class Nav extends React.Component {
                         <NavbarBrand href="/">
                             <strong>React Football</strong>
                         </NavbarBrand>
-                        <NavbarToggler onClick={this.onClick}/>
+                        <NavbarToggler/>
                         <Collapse isOpen={this.state.collapse} navbar>
                             <NavbarNav left>
                                 {config.menuOptions.map(item => (
